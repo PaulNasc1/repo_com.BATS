86cbaf4b98c05a211e07e77ed244a7e927793a90
Paulo Ricardo <paulockp1998@icloud.com>
2024-08-15 21:38:06 -0300

    Add files via upload

---
+++ Apaga_Temps.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Limpando arquivos temporários...
+
+CD\
+CD %TEMP%
+DEL P*.LCK /S /Q
+DEL _QS*.* /S /Q
+DEL *TMP*.* /S /Q
+
+CD\
+CD C:\Temp
+DEL P*.LCK /S /Q
+DEL _QS*.* /S /Q
+DEL *TMP*.* /S /Q
+
+CD EQUIPEXE
+DEL P*.LCK /S
+DEL _QS*.* /S
+DEL *TMP*.* /S
+CD\
+DEL C:\PDOXUSRS.NET
+
+CD\
+DEL C:\PDOXUSRS.NET /Q
+
+PAUSE
+
+echo Limpeza concluída.
