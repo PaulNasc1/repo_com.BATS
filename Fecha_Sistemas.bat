3dfd32a6652ab58f309b2418da1e503b6312a2f8
Paulo Ricardo <paulockp1998@icloud.com>
2024-08-15 21:40:12 -0300

    Update Fecha_Sistemas.bat

--- Fecha_Sistemas.bat
+++ Fecha_Sistemas.bat
@@ -20,15 +20,3 @@ taskkill /f /im Boletos.exe
 cls
 taskkill /f /im LiveUpdate.exe
 cls
-
-C:
-CD\
-CD TEMP
-DEL TMP*.* /S
-CD\
-CD EQUIPEXE
-DEL P*.LCK /S
-DEL _QS*.* /S
-DEL TMP*.* /S
-CD\
-DEL C:\PDOXUSRS.NET 
